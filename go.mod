a5eb88bd43ae89b2f8c4043ad643c7067de0991c
gaiad9999 <jsm3874aa@gmail.com>
2022-10-25 00:38:23 +0900

    Mux test 22/10/25/00:38

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gaiad9999/goElementary
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- goMux/MuxBasic1.go
+++ goMux/MuxBasic1.go
@@ -2,9 +2,18 @@ package goMux
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // 여기에서는 파일 컨트롤에 대한 내용을 추가할 예정
 func Cache() {
 	fmt.Println("testing mux")
+}
+
+func StartHandler() http.Handler {
+	mux := mux.NewRouter()
+
+	return mux
 }
